d5e4b0a0fa57afed69c8f69f5be4eb44919ecae6
ikealmighty <isaacyates7@gmail.com>
2022-07-21 12:25:39 -0500

    wrote status 200 test for /api/jar/put

--- tests/api/jar/put.spec.js
+++ tests/api/jar/put.spec.js
@@ -1,2 +1,10 @@
 // test for putting a text in the jar!
 const { test, expect } = require("@playwright/test");
+
+test("test-api-jar-put", async ({ request }) => {
+  let response = await request.post("http://localhost:3000/api/jar/put", {
+    data: JSON.stringify({ text: "test message" }),
+  });
+
+  expect(response.status === 200);
+});
